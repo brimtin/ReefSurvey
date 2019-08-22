3d800aea7d992ca41a7f5cf32aaab889a5b2b16e
Thomas Lowry <tomlowry@gmail.com>
2019-08-21 23:56:29 -0400

    Database implementation

---
+++ directory/ConsoleApp.SQLite/Migrations/20190822024239_InitialCreate.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp.SQLite.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FishInfos",
+                columns: table => new
+                {
+                    FishInfoID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MetricID = table.Column<int>(nullable: false),
+                    FishLength = table.Column<float>(nullable: false),
+                    StructureType = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FishInfos", x => x.FishInfoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FishNames",
+                columns: table => new
+                {
+                    FishNameID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ScientificName = table.Column<string>(nullable: true),
+                    CommonName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FishNames", x => x.FishNameID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FishObserveds",
+                columns: table => new
+                {
+                    FishObservedID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MetricID = table.Column<int>(nullable: false),
+                    LocationID = table.Column<int>(nullable: false),
+                    Family = table.Column<string>(nullable: true),
+                    Trophic = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FishObserveds", x => x.FishObservedID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RegionID = table.Column<int>(nullable: false),
+                    Longitude = table.Column<float>(nullable: false),
+                    Latitude = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    RegionID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RegionName = table.Column<string>(nullable: true),
+                    SubRegionName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.RegionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Surveys",
+                columns: table => new
+                {
+                    SurveyID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ObservationID = table.Column<int>(nullable: false),
+                    SurveyIndex = table.Column<int>(nullable: false),
+                    BatchCode = table.Column<string>(nullable: true),
+                    StudyArea = table.Column<string>(nullable: true),
+                    SurveyDate = table.Column<string>(nullable: true),
+                    SurveyYear = table.Column<string>(nullable: true),
+                    Management = table.Column<string>(nullable: true),
+                    FishCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Surveys", x => x.SurveyID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FishInfos");
+
+            migrationBuilder.DropTable(
+                name: "FishNames");
+
+            migrationBuilder.DropTable(
+                name: "FishObserveds");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+
+            migrationBuilder.DropTable(
+                name: "Surveys");
+        }
+    }
+}
---
+++ directory/ConsoleApp.SQLite/Model.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ConsoleApp.SQLite
+{
+    public class ReefSurvey : DbContext
+    {
+        public DbSet<FishInfo> FishInfos { get; set; }
+        public DbSet<FishName> FishNames { get; set; }
+        public DbSet<FishObserved> FishObserveds { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Survey> Surveys { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=reefsurvey.db");
+        }
+    }
+
+    public class FishInfo
+    {
+        public int FishInfoID { get; set; }
+        public int MetricID { get; set; }
+        public float FishLength { get; set; }
+        public string StructureType { get; set; }
+    }
+
+    public class FishName
+    {
+        public int FishNameID { get; set; }
+        public string ScientificName { get; set; }
+        public string CommonName { get; set; }
+    }
+
+    public class FishObserved
+    {
+        public int FishObservedID { get; set; } // Observation
+        public int MetricID { get; set; }
+        public int LocationID { get; set; }
+        public string Family { get; set; }
+        public string Trophic { get; set; }
+    }
+
+    public class Location
+    {
+        public int LocationID { get; set; }
+        public int RegionID { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+    }
+
+    public class Region
+    {
+        public int RegionID { get; set; }
+        public string RegionName { get; set; }
+        public string SubRegionName { get; set; }
+    }
+
+    public class Survey
+    {
+        public int SurveyID { get; set; }
+        public int ObservationID { get; set; }
+        public int SurveyIndex { get; set; }
+        public string BatchCode { get; set; }
+        public string StudyArea { get; set; }
+        public string SurveyDate { get; set; }
+        public string SurveyYear { get; set; }
+        public string Management { get; set; }
+        public int FishCount { get; set; }
+    }
+}
